36ee303a55e9cb3249ee272949e30781fc0fb1b1
mesho-17 <103313805+mesho-17@users.noreply.github.com>
2022-10-13 16:28:34 +0300

    Update password_gen.py

--- password_gen.py
+++ password_gen.py
@@ -1,14 +1,14 @@
-import string
-import random
+import string #python library/ module containing string constants.
+import random #random characters generator.
  
 def generate_password(size):
    all_chars = string.ascii_letters + string.digits + string.punctuation
    password = ''
-   for char in range(size):
-       rand_char = random.choice(all_chars)
-       password = password + rand_char
-   return password
+   for char in range(size): #after password size specification, the code will generate the password randomly.
+       rand_char = random.choice(all_chars) #creation of random characters
+       password = password + rand_char # or password += rand_char
+   return password #return value.
  
-pass_len = int(input('How many characters in your password?'))
-new_password = generate_password(pass_len)
+pass_len = int(input('How many characters in your password?')) 
+new_password = generate_password(pass_len) #pass_len is an argument passed to the 'generate_password' function. (the function call)
 print('Here is Your new password: ', new_password)
